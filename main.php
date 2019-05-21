e183881579f1f67951a81482b583e2520f080a3c
Masha <mashushvik.2001@gmail.com>
2019-05-21 22:41:48 +0300

    Optional second argument as filename

--- main.php
+++ main.php
@@ -5,8 +5,6 @@ require_once "RunLengthCoder.php";
 const INPUT_FILE = 'input.txt';
 const OUTPUT_FILE = 'output.txt';
 
-var_dump($argv);
-
 function read_command()
 {
     global $argv;
@@ -20,9 +18,10 @@ function read_command()
 
     } else if ($argv[1] === 'extract') {
         $input = $argv[2];
+        $outname = isset($argv[3]) ? $argv[3] : null;
 
         $decoder = new RunLengthCoder();
-        $decoder->decode($input);
+        $decoder->decode($input, $outname);
     } else {
         echo "Wrong command" . "\n";
         return;
